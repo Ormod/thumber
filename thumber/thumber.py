33f364766f0cb45dae2c2e2eb89b63a44bd3dc6f
Hannu Valtonen <hannu.valtonen@ormod.com>
2011-01-01 21:37:44 +0200

    Make thumber more pylint friendly and split to two classes so you can only use the index if that's all you want.

--- thumber/thumber.py
+++ thumber/thumber.py
@@ -1,6 +1,6 @@
+"""Thumber Library
+Author Hannu Valtonen"""
 import Image
-import hashlib
-import os
 import struct
 import StringIO
 try:
@@ -11,47 +11,51 @@ except:
 INDEX_VERSION = 1
 
 class Thumber(object):
-    def __init__(self, thumbnail_sizes = None, reserved_additional_keys = None, plugins = None):
+    """Thumber librarys main class, use this if you want to use everything"""
+    def __init__(self, thumbnail_sizes = None, reserved_keys = None, plugins = None):
         if thumbnail_sizes:
             self.thumbnail_sizes = thumbnail_sizes
         else:
-            self.thumbnail_sizes = ((128,128), (64,64), (32,32)) #default sizes
-        if reserved_additional_keys:
-            self.reserved_additional_keys = reserved_additional_keys
-        else:
-            self.reserved_additional_keys = []
+            self.thumbnail_sizes = ((128, 128), (64, 64), (32, 32)) #default sizes
         if plugins:
             self.plugins = plugins
         else:
             self.plugins = [self.create_thumbnails]
+        self.thumb_indexer = ThumberIndex(reserved_keys)
 
-    def create_thumbs_and_index(self, data_blob, reserved_additional_keys = None):
+    def create_thumbs_and_index(self, data_blob, extra_reserved_keys = None):
         """Create thumbnails and an index, and add possible additional keys to the file index"""
         result_dict = {}
         for plugin in self.plugins:
             result_dict.update(plugin(data_blob))
-        if reserved_additional_keys:
-            result_dict.update(reserved_additional_keys)
-        return self.create_thumbnail_blob_with_index(result_dict)
+        return self.thumb_indexer.create_thumbnail_blob_with_index(result_dict, extra_reserved_keys = extra_reserved_keys)
 
     def create_thumbnails(self, data_blob):
         """Create required thumbnails, sizes are set in object instance creation time"""
-        im = Image.open(StringIO.StringIO(data_blob))
+        image = Image.open(StringIO.StringIO(data_blob))
         result_dict = {}
         for thumbnail_size in self.thumbnail_sizes:
-            im.thumbnail(thumbnail_size, Image.ANTIALIAS)
+            image.thumbnail(thumbnail_size, Image.ANTIALIAS)
             file_buffer = StringIO.StringIO()
-            if im.mode != "RGB":
-                im = im.convert("RGB")
-            im.save(file_buffer, format = 'JPEG')
+            if image.mode != "RGB":
+                image = image.convert("RGB")
+            image.save(file_buffer, format = 'JPEG')
             result_dict[str(thumbnail_size[0]) + "x" + str(thumbnail_size[1]) + "xjpg"] = file_buffer.getvalue()
         return result_dict
 
-    def create_thumbnail_blob_with_index(self, result_dict):
+class ThumberIndex(object):
+    """Class for creating files with n thumbnails and an access index"""
+    def __init__(self, reserved_keys):
+        if reserved_keys:
+            self.reserved_keys = reserved_keys
+        else:
+            self.reserved_keys = []
+
+    def create_thumbnail_blob_with_index(self, result_dict, extra_reserved_keys = None):
         """Expects a dict like {'128x128xjpg': thumbnail_data, '64x64xjpg': thumbnail2_data, 'my_reserved_key': 1}"""
         header, data, current_offset = {'version': INDEX_VERSION}, [], 0
         for k, v in result_dict.items():
-            if k not in self.reserved_additional_keys:
+            if k not in self.reserved_keys:
                 offsets = [current_offset, current_offset + len(v)]
                 header[k] = offsets
                 current_offset += len(v)
@@ -63,7 +67,7 @@ class Thumber(object):
         data_list.extend(data)
         return ''.join(data_list)
 
-    def read_thumbnail_blob_with_index(self, data_blob, thumbnail_key = None):
+    def read_thumbnail_blob_with_index(self, data_blob, thumbnail_key = None, extra_reserved_keys = None):
         """Expects a data blob created by create_thumbnail_file_with_index
         If thumbnail_key has been given, returns the blob in question
         otherwise return a thumbnail_dict with all available thumbnail sizes
@@ -84,15 +88,16 @@ class Thumber(object):
                 return_dict[k] = v
         return return_dict
 
-def help():
+def help_msg():
+    """Help msg function for thumber"""
     print "Help:"
     print "    thumber store <input_image_file> <output_thumbnail_index_file>"
     print "    thumber load <input_thumbnail_index> <size> <output_thumbnail_image.jpg>"
     print "Thumber Copyright Hannu Valtonen 2011"
 
 def main():
-    import sys, time
-    start_time = time.time()
+    """Entry point for thumber console script"""
+    import sys
     if len(sys.argv) >= 4:
         a = Thumber()
         input_data = file(sys.argv[2], "rb").read()
@@ -100,17 +105,16 @@ def main():
             data_blob = a.create_thumbs_and_index(input_data)
             output_filename = sys.argv[3]
         elif sys.argv[1] == "load" and len(sys.argv) == 5:
-            data_blob = a.read_thumbnail_blob_with_index(input_data, sys.argv[3] + "xjpg")
+            data_blob = a.thumb_indexer.read_thumbnail_blob_with_index(input_data, sys.argv[3] + "xjpg")
             output_filename = sys.argv[4]
         else:
-            help()
+            help_msg()
             sys.exit(0)
         output_file = file(output_filename, "wb")
         output_file.write(data_blob)
         output_file.close()
     else:
-        help()
-    print "Thumber took %.3fs to run" % (time.time() - start_time)
+        help_msg()
 
 if __name__ == "__main__":
     main()
